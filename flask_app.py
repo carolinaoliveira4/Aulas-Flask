0a6e5254dc5677298074179dbc0bcdc0ab2d479d
carolinaoliveira4 <mariacarolina.oliveira204@gmail.com>
2024-10-16 18:46:44 +0000

    Aula 040

--- flask_app.py
+++ flask_app.py
@@ -2,36 +2,50 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, request, redirect, make_response, abort, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-@app.route('/user/<name>/<prontuario>/<inst>')
-def user(name, prontuario, inst):
-    return render_template('identificacao.html', aluno = name, prontuario = prontuario, inst = inst)
+    return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/contextorequisicao')
-def contextorequisicao():
-    user_agent = request.headers.get('User-Agent')
-    return render_template('contexto.html', navegador = user_agent, ip = request.remote_addr, host = request.url)
+@app.route('/user/<user>')
+def hello_user(user):
+    return render_template('user.html', user = user)
 
-@app.route('/redirecionamento')
-def redirecionamento():
-    return redirect('https://ptb.ifsp.edu.br/', code=302) # Se questionar porquê do código 302, é porque ele é o código HTTP que informa ser um redirecionamento
+@app.errorhandler(404)
+def page_not_found(e):
+ return render_template('404.html'), 404
 
-@app.route('/objetoresposta')
-def cookie():
-    resposta = make_response('<h1>This document carries a cookie!</h1>')
-    resposta.set_cookie('Exemplo_cookie', "Positivo")
-    return resposta
+@app.errorhandler(500)
+def internal_server_error(e):
+ return render_template('500.html'), 500
 
-@app.route('/codigostatusdiferente')
-def codigo_status_diferente():
-    return 'DESCOBRIR'
+# @app.route('/contextorequisicao')
+# def contextorequisicao():
+#     user_agent = request.headers.get('User-Agent')
+#     return render_template('contexto.html', navegador = user_agent, ip = request.remote_addr, host = request.url)
 
-@app.route('/abortar')
-def abortar():
-    abort(404)
+# @app.route('/redirecionamento')
+# def redirecionamento():
+#     return redirect('https://ptb.ifsp.edu.br/', code=302) # Se questionar porquê do código 302, é porque ele é o código HTTP que informa ser um redirecionamento
+
+# @app.route('/objetoresposta')
+# def cookie():
+#     resposta = make_response('<h1>This document carries a cookie!</h1>')
+#     resposta.set_cookie('Exemplo_cookie', "Positivo")
+#     return resposta
+
+# @app.route('/codigostatusdiferente')
+# def codigo_status_diferente():
+#     return 'DESCOBRIR'
+
+# @app.route('/abortar')
+# def abortar():
+#     abort(404)
 
